72fcb18469ebfb2e20aba1f5bee22ff6852cf73f
VicCham98 <41102257+VicCham98@users.noreply.github.com>
2021-10-03 13:13:16 -0500

    function get all divisiones

--- app/Http/Controllers/DivisionesController.php
+++ app/Http/Controllers/DivisionesController.php
@@ -2,13 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use App\Model\Divisiones;
+use App\Model\DivisionesContent;
+use App\Model\SubDivisionContent;
 use Illuminate\Http\Request;
 
 class DivisionesController extends Controller
 {
     public function divisiones(){
-        $divisiones = Divisiones::get();
+        $divisiones = DivisionesContent::where([])
+        ->join('divisiones', 'divisiones.id', 'divisiones_content.divisiones_id')
+        ->join('division_superior', 'division_superior.id', 'divisiones_content.division_superior_id')
+        ->select(
+            'divisiones_content.id as divisionContentId',
+            'divisiones.id as divisionId',
+            'divisiones.div_nombre as division',
+            'divisiones.div_nivel as nivel',
+            'divisiones.div_cantidad as colaboradores',
+            'divisiones.div_embajador as embajador',
+            'division_superior.div_sup_nombre as divisionSup',
+            'division_superior.id as divisionSupId',
+        )
+        ->get();
+
+        foreach ($divisiones as $key => $value) {
+            $divisiones[$key]['subDivision'] = SubDivisionContent::where([
+                ['divisiones_id', $value['divisionId']]
+            ])
+            ->count();
+            $divisiones[$key]['key'] = $value['divisionId'];
+        }
 
         return response()->json([
             'success' => true,
--- config/app.php
+++ config/app.php
@@ -1,4 +1,8 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: *');
+
 
 return [
 
